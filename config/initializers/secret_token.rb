b4f5fe20d161e48412acd081eed8219915d0fa71
Jon Nguyen <bobfet1@Jons-MacBook-Air.local>
2011-08-06 11:25:49 -0700

    bartapper

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bartapper::Application.config.secret_token = '2e7136b53f4e393daf1631bcd0a822937dbbe48d6752094480ac51685f4392ec82991dc69ae7c3f5b6e5559dbda3324f956878a19df2d76bc4e62c684044781e'
